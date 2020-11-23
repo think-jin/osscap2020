1b8540177de80d6173305e3ab95e7dd2d7046a74
think-jin <thingjin@naver.com>
2020-11-23 17:06:02 +0900

    pytet

---
+++ pytet_v0.3/tetris.py
@@ -0,0 +1,86 @@
+from matrix import *
+from random import *
+from enum import Enum
+#import LED_display as LMD 
+
+class TetrisState(Enum):
+    Running = 0
+    NewBlock = 1
+    Finished = 2
+### end of class TetrisState():
+
+class Tetris():
+    nBlockTypes = 0
+    nBlockDegrees = 0
+    setOfBlockObjects = 0
+    iScreenDw = 0   # larget enough to cover the largest block
+
+    @classmethod
+    def init(cls, setOfBlockArrays):
+        Tetris.nBlockTypes = len(setOfBlockArrays)
+        Tetris.nBlockDegrees = len(setOfBlockArrays[0])
+        Tetris.setOfBlockObjects = [[0] * Tetris.nBlockDegrees for _ in range(Tetris.nBlockTypes)]
+        arrayBlk_maxSize = 0
+        for i in range(Tetris.nBlockTypes):
+            if arrayBlk_maxSize <= len(setOfBlockArrays[i][0]):
+                arrayBlk_maxSize = len(setOfBlockArrays[i][0])
+        Tetris.iScreenDw = arrayBlk_maxSize     # larget enough to cover the largest block
+
+        for i in range(Tetris.nBlockTypes):
+            for j in range(Tetris.nBlockDegrees):
+                Tetris.setOfBlockObjects[i][j] = Matrix(setOfBlockArrays[i][j])
+        return
+		
+    def createArrayScreen(self):
+        self.arrayScreenDx = Tetris.iScreenDw * 2 + self.iScreenDx
+        self.arrayScreenDy = self.iScreenDy + Tetris.iScreenDw
+        self.arrayScreen = [[0] * self.arrayScreenDx for _ in range(self.arrayScreenDy)]
+        for y in range(self.iScreenDy):
+            for x in range(Tetris.iScreenDw):
+                self.arrayScreen[y][x] = 1
+            for x in range(self.iScreenDx):
+                self.arrayScreen[y][Tetris.iScreenDw + x] = 0
+            for x in range(Tetris.iScreenDw):
+                self.arrayScreen[y][Tetris.iScreenDw + self.iScreenDx + x] = 1
+
+        for y in range(Tetris.iScreenDw):
+            for x in range(self.arrayScreenDx):
+                self.arrayScreen[self.iScreenDy + y][x] = 1
+
+        return self.arrayScreen
+		
+    def __init__(self, iScreenDy, iScreenDx):
+        self.iScreenDy = iScreenDy
+        self.iScreenDx = iScreenDx
+        self.idxBlockDegree = 0
+        arrayScreen = self.createArrayScreen()
+        self.iScreen = Matrix(arrayScreen)
+        self.oScreen = Matrix(self.iScreen)
+        self.justStarted = True
+        return
+
+    def printScreen(self):
+        array = self.oScreen.get_array()
+
+        for y in range(self.oScreen.get_dy()-Tetris.iScreenDw):
+            for x in range(Tetris.iScreenDw, self.oScreen.get_dx()-Tetris.iScreenDw):
+                if array[y][x] == 0:
+                    print("□", end='')
+                    #LMD.set_pixel(y, 19-x, 0)
+                elif array[y][x] == 1:
+                    print("■", end='')
+                    #LMD.set_pixel(y, 19-x, 4)
+                else:
+                    print("XX", end='')
+                    #continue
+            print()
+
+    def deleteFullLines(self): # To be implemented!!
+        return
+
+    def accept(self, key): # To be implemented!!
+        self.state = TetrisState.Running
+        return self.state
+
+### end of class Tetris():
+    
