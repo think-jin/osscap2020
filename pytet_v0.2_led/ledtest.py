ce7c5ddfc1d0f91222ebf9ee6f32ccbd11d08a86
think-jin <70687311+think-jin@users.noreply.github.com>
2020-11-23 15:33:10 +0900

    Add files via upload

---
+++ pytet_v0.2_led/ledtest.py
@@ -0,0 +1,10 @@
+import LED_display as LMD
+import threading
+
+def LED_init():
+    thread=threading.Thread(target=LMD.main, args=())
+    thread.setDaemon(True)
+    thread.start()
+    return
+
+LMD.set_pixel(5, 5, 0)
